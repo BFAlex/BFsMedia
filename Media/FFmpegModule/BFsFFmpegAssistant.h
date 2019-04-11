3a949327385be20aac6df57eba0095f3a92cd85b
Alex BF <alexbf@AlexdeMac.local>
2019-04-12 00:08:06 +0800

    20190412

---
+++ Media/FFmpegModule/BFsFFmpegAssistant.h
@@ -0,0 +1,16 @@
+//
+//  BFsFFmpegAssistant.h
+//  Media
+//
+//  Created by Alex BF on 2019/4/3.
+//  Copyright © 2019年 BFs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFsFFmpegAssistant : NSObject
+
++ (instancetype)sharedInstance;
+- (void)destoryInstance;
+
+@end
